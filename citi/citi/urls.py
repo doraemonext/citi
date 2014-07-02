c9b71bbbfe4ccf2b4fddf6ba6dc2297a86ca19e9
doraemonext <doraemonext@gmail.com>
2014-07-02 22:21:21 +0800

    删除xadmin

--- citi/citi/urls.py
+++ citi/citi/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
 
-import xadmin
-xadmin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(xadmin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 # Uncomment the next line to serve media files in dev.
