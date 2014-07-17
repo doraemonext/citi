eac4448995ad455082ef38adfe377f6b912f8fae
doraemonext <doraemonext@gmail.com>
2014-07-17 15:04:12 +0800

    增加设置描述, 完善管理界面显示

--- citi/system/settings/management/__init__.py
+++ citi/system/settings/management/__init__.py
@@ -7,8 +7,8 @@ from system.settings.models import Settings
 
 
 def settings_initial(sender, **kwargs):
-    Settings.objects.create(name='title', value=u'好味道众筹')
-    Settings.objects.create(name='description', value=u'好味道众筹的描述')
-    Settings.objects.create(name='keywords', value=u'好味道众筹的关键字')
+    Settings.objects.create(name='title', description=u'网站标题', value=u'好味道众筹')
+    Settings.objects.create(name='description', description=u'网站描述', value=u'好味道众筹的描述')
+    Settings.objects.create(name='keywords', description=u'网站关键字', value=u'好味道众筹的关键字')
 
 post_syncdb.connect(settings_initial, sender=system.settings.models)
--- citi/system/settings/models.py
+++ citi/system/settings/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Settings(models.Model):
-    name = models.CharField(u'设置名称', max_length=30)
+    name = models.CharField(u'设置名称', max_length=30, editable=False)
+    description = models.CharField(u'设置描述', max_length=255, editable=False)
     value = models.TextField(u'设置内容')
 
     class Meta:
@@ -13,3 +14,6 @@ class Settings(models.Model):
         permissions = (
             ('view_settings', u'Can view 系统设置'),
         )
+
+    def __unicode__(self):
+        return self.description
