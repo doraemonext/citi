5654189e63d5d6d1b68a0d11e8363df2d0f28e9b
doraemonext <doraemonext@gmail.com>
2014-07-19 15:50:31 +0800

    修正两套API权限

--- citi/apps/crowdfunding/api.py
+++ citi/apps/crowdfunding/api.py
@@ -3,6 +3,7 @@
 import logging
 
 from rest_framework import generics
+from rest_framework import permissions
 
 from libs.api import mixins
 from .models import ProjectFeedback, ProjectPackage
@@ -13,6 +14,7 @@ class ProjectFeedbackList(mixins.CustomCreateModelMixin,
                           generics.GenericAPIView):
     queryset = ProjectFeedback.objects.all()
     serializer_class = ProjectFeedbackSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
@@ -24,6 +26,7 @@ class ProjectFeedbackDetail(mixins.CustomRetrieveModelMixin,
                             generics.GenericAPIView):
     queryset = ProjectFeedback.objects.all()
     serializer_class = ProjectFeedbackSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -39,6 +42,7 @@ class ProjectPackageList(mixins.CustomCreateModelMixin,
                          generics.GenericAPIView):
     queryset = ProjectPackage.objects.all()
     serializer_class = ProjectPackageSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
@@ -50,6 +54,7 @@ class ProjectPackageDetail(mixins.CustomRetrieveModelMixin,
                            generics.GenericAPIView):
     queryset = ProjectPackage.objects.all()
     serializer_class = ProjectPackageSerializer
+    permission_classes = (permissions.IsAuthenticated, )
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
