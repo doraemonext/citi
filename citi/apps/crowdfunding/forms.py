72af99a1588286c5ec8a5b0df751c8e061ba6ccd
doraemonext <doraemonext@gmail.com>
2014-07-20 21:25:03 +0800

    修正用户字段

--- citi/apps/crowdfunding/forms.py
+++ citi/apps/crowdfunding/forms.py
@@ -3,7 +3,7 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import Project, ProjectCover, ProjectCategory
+from .models import Project, ProjectCategory
 
 
 class ProjectForm(forms.ModelForm):
