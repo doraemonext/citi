3d4ec5b5973eed85afd4cbb99747dc534f7da188
doraemonext <doraemonext@gmail.com>
2014-09-04 20:00:03 +0800

    Add: 个人中心页面完成

--- citi/apps/account/api.py
+++ citi/apps/account/api.py
@@ -32,6 +32,9 @@ class UserDetail(mixins.CustomRetrieveModelMixin,
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
 
