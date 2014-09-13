7faf101edf58f1a09108d4de84d14e00db40be43
doraemonext <doraemonext@gmail.com>
2014-09-13 23:44:23 +0800

    fix

--- citi/apps/home/views.py
+++ citi/apps/home/views.py
@@ -19,20 +19,20 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['config'] = Settings.manager.get_setting_dict()
         context['categories'] = ProjectCategory.objects.all()
-        context['local_projects'] = Project.objects.order_by('-post_datetime')[:4]
+        context['local_projects'] = Project.objects.filter(status__gt=3).order_by('-post_datetime')[:4]
         for project in context['local_projects']:
             project.image = get_thumbnail(Image.objects.get(pk=project.cover).image, '239x200', crop='center').url
         context['today_best'] = []
         for category in context['categories']:
             try:
-                context['today_best'].append(Project.objects.filter(category=category).order_by('-attention_count')[0])
+                context['today_best'].append(Project.objects.filter(category=category).filter(status__gt=3).order_by('-attention_count')[0])
                 context['today_best'][-1].image = get_thumbnail(Image.objects.get(pk=context['today_best'][-1].cover).image, '560x400', crop='center').url
             except IndexError:
                 pass
-        context['hot_projects'] = Project.objects.order_by('-attention_count')[:4]
+        context['hot_projects'] = Project.objects.filter(status__gt=3).order_by('-attention_count')[:4]
         for project in context['hot_projects']:
             project.image = get_thumbnail(Image.objects.get(pk=project.cover).image, '239x200', crop='center').url
-        context['latest_projects'] = Project.objects.order_by('-post_datetime')[:4]
+        context['latest_projects'] = Project.objects.filter(status__gt=3).order_by('-post_datetime')[:4]
         for project in context['latest_projects']:
             project.image = get_thumbnail(Image.objects.get(pk=project.cover).image, '239x200', crop='center').url
         return context
